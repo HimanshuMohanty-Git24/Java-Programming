f5a45669655c723f013a6c1e705c9279ff38fd7b
Himanshu Mohanty <codehimanshu24@gmail.com>
2023-02-11 11:55:58 +0530

    Main

--- CP/Main.java
+++ CP/Main.java
@@ -5,3 +5,13 @@ public class Main {
         System.out.println("Hello World!");
     }
 }
+// Public:So that it can be accessed by all the user
+// static:Main function is the entry point of the program so it needs to be run
+// this function without creating an object of main class so we make it static
+// void: Because this function is not returning any thing to the user
+// main: This is the name of the function
+// String[] args: This is the parameter of the function
+// System.out.println("Hello World!");: This is the body of the function
+// System: This is the class
+// out: This is the object of the class
+// println: This is the method of the class
