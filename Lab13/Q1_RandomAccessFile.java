83cf86bcc1127cf220d930fc86a31aaedf453752
Himanshu Mohanty <codehimanshu24@gmail.com>
2023-04-18 13:01:46 +0530

    Tokenizer

--- Lab13/Q1_RandomAccessFile.java
+++ Lab13/Q1_RandomAccessFile.java
@@ -7,7 +7,7 @@ class Q1_RandomAccessFile {
     public static void main(String args[]) throws IOException {
         RandomAccessFile raf = new RandomAccessFile("Q1.txt", "rw");
         raf.writeBytes("Hello World");
-        raf.seek(0);
+        raf.seek(4);
         System.out.println(raf.readLine());
         raf.close();
     }
